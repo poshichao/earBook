78ef00053b747cf67b8079658e27f0b9dc1082e1
chaoweb <1648907448@qq.com>
2019-03-26 16:23:29 +0800

    添加后台依赖

---
+++ earBook/src/main/java/com/android/earBook/EarBookApplication.java
@@ -0,0 +1,13 @@
+package com.android.earBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarBookApplication.class, args);
+	}
+
+}
